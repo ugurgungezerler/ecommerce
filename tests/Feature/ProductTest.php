dfa003dbf4a500c89525694c0b7ea3234132598a
Ugur Gungezerler <ugur@peakode.com>
2021-02-10 14:42:17 +0300

    add: product tests

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -2,14 +2,58 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    public function test_list_products()
+    use DatabaseMigrations, WithFaker;
+
+
+    public function testListProducts()
     {
-        $response = $this->get('/api/product');
+        Product::factory(4)->create();
+
+        $response = $this->get('/api/products');
+
+        $response->assertStatus(200)->assertJsonFragment(['total' => 4]);
+    }
+
+    public function testShowProduct()
+    {
+        Product::factory(4)->create();
+
+        $response = $this->get('/api/products/1');
+
+        $response->assertStatus(200)->assertJsonFragment(['id' => 1]);
+    }
+
+    public function testUpdateProduct()
+    {
+        Product::factory(4)->create();
+
+        $response = $this->patch('/api/products/1',
+            [
+                'name' => 'Updated Product',
+                'description' => $this->faker->sentence(4),
+                'price' => '10.99',
+                'image' => $this->faker->imageUrl(500, 500),
+            ]);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment([
+                'id' => 1,
+                'name' => 'Updated Product'
+            ]);
+    }
+
+    public function testDestroyProduct()
+    {
+        Product::factory(4)->create();
+
+        $response = $this->delete('/api/products/1');
 
         $response->assertStatus(200);
     }
