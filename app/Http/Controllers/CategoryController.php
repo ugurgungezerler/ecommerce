7a2d5dd72fba26a24b6de6d1d6fc1dba431367e7
Ugur Gungezerler <ugur@peakode.com>
2021-02-10 14:39:58 +0300

    refactor: unused use statement

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,7 +6,6 @@ use App\Http\Requests\StoreCategoryRequest;
 use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
