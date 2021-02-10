60f443b1a938304a2b9e8fbb8d823587ee069787
Ugur Gungezerler <ugur@peakode.com>
2021-02-10 14:22:20 +0300

    improvement: category services

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
 use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
+
+    /**
+     * @return JsonResponse
+     */
     public function index()
     {
-        return Category::paginate(5);
+        return response()->json(Category::paginate(5));
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function show($id)
+    {
+        return response()->json(Category::findOrFail($id));
+    }
+
+    /**
+     * @param StoreCategoryRequest $request
+     * @return mixed
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = Category::create($request->only('name'));
+
+        return response()->json($category);
+    }
+
+    /**
+     * @param UpdateCategoryRequest $request
+     * @param $id
+     * @return JsonResponse
+     */
+    public function update(UpdateCategoryRequest $request, $id)
+    {
+        $category = Category::findOrFail($id);
+
+        $category->update($request->only('name'));
+
+        return response()->json($category);
+    }
+
+    /**
+     * @param $id
+     * @return JsonResponse
+     */
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+
+        $category->delete();
+
+        return response()->json(['message' => 'Category successfully deleted']);
     }
 }
