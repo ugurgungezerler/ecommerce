ac02e4ca6cc9b33d771a2abe0f82ab1660af524d
Ugur Gungezerler <ugur@peakode.com>
2021-02-10 13:41:54 +0300

    add: categories

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resource('category', CategoryController::class);
+Route::resource('product', ProductController::class);
