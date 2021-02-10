b9277c2925830055774ae560a7dbb31f1c153829
Ugur Gungezerler <ugur@peakode.com>
2021-02-10 13:49:16 +0300

    improvement: routes

--- routes/api.php
+++ routes/api.php
@@ -15,5 +15,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('category', CategoryController::class);
-Route::resource('product', ProductController::class);
+Route::resource('category', CategoryController::class)->only([
+    'index',
+    'show',
+    'store',
+    'update',
+    'destroy',
+]);
+
+Route::resource('products', ProductController::class)->only([
+    'index',
+    'show',
+    'store',
+    'update',
+    'destroy',
+]);
